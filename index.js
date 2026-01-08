c434b1f86b9958409600ab8dfa51e48582e74e36
saifur-sagor <mdsaifurrahmansagor606@gmail.com>
2026-01-08 17:01:56 +0600

    problem-10

--- index.js
+++ index.js
@@ -117,3 +117,21 @@ function factorial(num) {
 }
 const factorialValue = factorial(3);
 console.log({ factorialValue });
+
+// problem-10
+function pingPong() {
+  for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("PingPong");
+    } else if (i % 3 === 0) {
+      console.log("Ping");
+    } else if (i % 5 === 0) {
+      console.log("Pong");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+pingPong();
+
